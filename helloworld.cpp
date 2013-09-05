be92a2f287fff41fdbdbdb0da2102c8e701ed0fe
Mikezhiwei <lzw00000@qq.com>
2013-09-05 22:53:13 +0800

    2013/9/5
    homework

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+// helloworld.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(int argc, _TCHAR* argv[])
+{   
+	cout<<"Hello World "<<"I am Mark"<<endl;
+	system("pause");
+	return 0;
+}
+
