4056ad258eaf900b94e20aabfef2b07cc4fcd63d
Mikezhiwei <lzw00000@qq.com>
2013-11-24 00:18:18 +0800

    教职工管理系统
    体现了C++的继承，虚函数多态，类型兼容，
    也体现了类的独立性和封装性

---
+++ 实验/h文件/Professor.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "cstaff.h"
+class CProfessor :
+	public Cstaff
+{
+public:
+	CProfessor(double d);
+	~CProfessor(void);
+	double getWage();
+	void showInfo();
+	string getRange();
+private:
+	int range;
+	double courefee;
+	int graCount;
+	int phdCount;
+	int cousehour;
+	double graHelp;
+	double phdHelp;
+	double Ywage;
+};
+
